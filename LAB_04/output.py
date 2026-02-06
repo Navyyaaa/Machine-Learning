e4f2fc7d5bb18a8569b427b76c123e34ba10fb48
Navyyaaa <navyasreevaddireddy@gmail.com>
2026-02-06 09:35:31 -0800

    Add files via upload

---
+++ LAB_04/output.py
@@ -0,0 +1,17 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============ RESTART: D:/Sem4/ml/lab/assignments/done/LAB04_CODE.py ============
+TRAINING RESULTS
+[[1091    7    0]
+ [   8 1087    0]
+ [   0    2 1162]]
+0.9949405692653235 0.9949359547214776 0.9949378711372844
+TESTING RESULTS
+[[259  13   0]
+ [ 12 279   0]
+ [  0   3 274]]
+0.96687222465445 0.9666666666666667 0.9667433046582833
+Not enough numeric features to plot scatter graph.
+Best k: {'n_neighbors': 1}
+Best CV Score: 0.9964250230643673
