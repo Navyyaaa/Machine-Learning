5fa4db318733d89055e61dccc716495ee6ac27e6
Navyyaaa <navyasreevaddireddy@gmail.com>
2026-03-03 01:55:46 -0800

    Add files via upload

---
+++ LAB_05/output.py
@@ -0,0 +1,38 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============= RESTART: D:\Sem4\ml\lab\assignments\done\LAB05\Q1.py =============
+Model trained successfully
+>>> 
+============= RESTART: D:\Sem4\ml\lab\assignments\done\LAB05\q2.py =============
+TRAIN METRICS
+MSE: 0.6551885298972705
+RMSE: 0.8094371685914049
+MAPE: 44.71530601316021
+R2: 0.013690209446538648
+
+TEST METRICS
+MSE: 0.6906435497875885
+RMSE: 0.8310496674613308
+MAPE: 47.901340068379675
+R2: -0.002219255818892263
+>>> 
+============= RESTART: D:\Sem4\ml\lab\assignments\done\LAB05\q3.py =============
+TRAIN METRICS (All Attributes)
+MSE: 0.3105422761990693
+RMSE: 0.5572632019064863
+MAPE: 29.511823290784317
+R2: 0.5325148817182076
+
+TEST METRICS (All Attributes)
+MSE: 0.6229424723080391
+RMSE: 0.7892670475244986
+MAPE: 42.92590778008869
+R2: 0.09602436567088957
+>>> 
+============= RESTART: D:\Sem4\ml\lab\assignments\done\LAB05\q5.py =============
+Silhouette Score: 0.06611914984993979
+Calinski-Harabasz Score: 310.0823367451402
+Davies-Bouldin Index: 3.6031830446090054
+>>> 
+============= RESTART: D:\Sem4\ml\lab\assignments\done\LAB05\q6.py =============
